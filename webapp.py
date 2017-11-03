90ed049d2363f1cb5cc699b60312a612f598d32b
franny1615 <31251485+franny1615@users.noreply.github.com>
2017-11-03 09:35:19 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -23,8 +23,8 @@ def get_fact(state):
         if state == c["State"]:
             fact += c["Age"]["Percent Under 18 Years"]
             numCounties += 1
-    fact = fact/numCounties
-    funfact = Markup("<p>" + "Percent of People Under the age of 18 Years in: " + state + " " +str(fact) + "</p>")
+    fact = round(fact/numCounties)
+    funfact = Markup("<p>" + "Percent of People Under the age of 18 Years in: " + state + " is, " + str(fact) + "</p>")
     return funfact
 
 @app.route("/")
